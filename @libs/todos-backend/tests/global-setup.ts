0ad7a7d87d797a6b4766894055ebfba8b3e860e7
remadex <s.cardon@outlook.com>
2026-02-13 15:05:21 +0100

    wip

--- @libs/todos-backend/tests/global-setup.ts
+++ @libs/todos-backend/tests/global-setup.ts
@@ -6,7 +6,7 @@ import { TestModule } from "./utils/setup-module.ts";
 
 let container: StartedPostgreSqlContainer;
 
-async function setup() {
+export async function setup() {
   container = await new PostgreSqlContainer("postgres:16-alpine")
     .withDatabase("test_db")
     .withUsername("test_user")
@@ -35,15 +35,8 @@ async function setup() {
   await orm.close();
 }
 
-async function teardown() {
+export async function teardown() {
   if (container) {
     await container.stop();
   }
-}
-
-export default async function () {
-  return {
-    setup,
-    teardown,
-  };
 }
